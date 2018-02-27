9fb52a4c938c00a1b1b60689cf8f45348793fdc3
Henrique Valcanaia <henriqueindalencio@gmail.com>
2018-02-27 05:56:01 -0300

    P07 (**) Flatten a nested linked list structure

---
+++ p07_henriqueiv.swift
@@ -0,0 +1,27 @@
+extension List where T: Equatable {
+    /** P07 (**) Flatten a nested linked list structure.
+     - returns: List<T> the list flattened
+     - author: Henrique Valcanaia (`henriqueindalencio@gmail.com`)
+     - complexity: unknown. It varies according to nesting level and number of elements
+     - difficulty: 2/3
+     */
+    func flatten() -> List<T> {
+        let array = dataFlatten()
+        return List(array)!
+    }
+    
+    private func dataFlatten() -> [T] {
+        var result = [T]()
+        var currentNode: List<T>? = self
+        while (currentNode != nil) {
+            if let listValue = currentNode!.value as? List<T> {
+                result.append(contentsOf: listValue.dataFlatten())
+            } else {
+                result.append(currentNode!.value)
+            }
+            currentNode = currentNode!.nextItem
+        }
+        
+        return result
+    }
+}
